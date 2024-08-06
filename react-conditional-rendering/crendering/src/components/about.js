e1a516df7df2d914b4da05f0a06615e499844c43
Frankline Were <franklinegift@gmail.com>
2024-08-06 09:26:00 +0300

    correct conditional rendering

--- react-conditional-rendering/crendering/src/components/about.js
+++ react-conditional-rendering/crendering/src/components/about.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Toyota({ speed, colour, model }) {
     return (
@@ -18,25 +18,50 @@ function Subaru({ speed, colour, model }) {
     );
 }
 
+function About() {
+    const [selectedCar, setSelectedCar] = useState("");
 
-function About({ selectedCar }) {
+    const handleCarSelection = (car) => {
+        setSelectedCar(car);
+    };
+
+    const renderSelectedCar = () => {
+        if (selectedCar === "Toyota") {
+            return (
+                <div>
+                    <img src="public/images/Luxury car garage.jpg" alt="Toyota Noah" />
+                    <Toyota speed="120Km/hr" colour="Red" model="Noah" />
+                </div>
+            );
+        } else if (selectedCar === "Mazda") {
+            return (
+                <div>
+                    <img src="/api/placeholder/300/200" alt="Mazda Demio" />
+                    <Mazda speed="120Km/hr" colour="Black" model="Demio" />
+                </div>
+            );
+        } else if (selectedCar === "Subaru") {
+            return (
+                <div>
+                    <img src="/api/placeholder/300/200" alt="Subaru Impreza" />
+                    <Subaru speed="220Km/hr" colour="Blue" model="Impreza" />
+                </div>
+            );
+        } else {
+            return null;
+        }
+    };
+
     return (
         <div id="about-us">
             <h2>About Us</h2>
             <p>What <strong>car</strong> do you want to drive?</p>
-            {selectedCar === "Toyota" && (
-                <div>
-                    <img src="" alt="Toyota Noah" />
-                    <Toyota speed="120Km/hr" colour="Red" model="Know Her" />
-                </div>)}
-            {selectedCar === "Mazda" && (
-                <div>
-                    <Mazda speed="120Km/hr" colour="Black" model="Dem Yow" />
-                </div>)}
-            {selectedCar === "Subaru" && (
-                <div>
-                    <Subaru speed="220Km/hr" colour="Blue" model="Impress Her" />
-                </div>)}
+            <div>
+                <button onClick={() => handleCarSelection("Toyota")}>Toyota</button>
+                <button onClick={() => handleCarSelection("Mazda")}>Mazda</button>
+                <button onClick={() => handleCarSelection("Subaru")}>Subaru</button>
+            </div>
+            {renderSelectedCar()}
         </div>
     );
 }
